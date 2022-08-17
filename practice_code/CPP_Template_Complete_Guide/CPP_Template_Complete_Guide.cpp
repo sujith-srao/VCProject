40aac915cd3cf519cf288edbc4692f971bc3a3db
Sujith Rao <sujith.srao@gmail.com>
2022-08-17 22:50:44 +0200

    Some more C++ template code

--- practice_code/CPP_Template_Complete_Guide/CPP_Template_Complete_Guide.cpp
+++ practice_code/CPP_Template_Complete_Guide/CPP_Template_Complete_Guide.cpp
@@ -7,6 +7,7 @@
 #include<stack>
 #include<algorithm>
 #include<list>
+#include<array>
 using namespace std;
 template <typename T,typename CONT>
 void fillCont(CONT &cont, T const& val) {
@@ -135,6 +136,233 @@ namespace CPP_8_3_1_2 {
         //test(mc);
     }
 }
+namespace CH_15_1 {
+    class My {
+
+    };
+    template<typename T>
+    class AccumulationTrait;
+
+    template<>
+    class AccumulationTrait<char> {
+    public:
+        typedef int AccumTrait;
+        static AccumTrait zero;
+    };
+
+    template<>
+    class AccumulationTrait<float> {
+    public:
+        typedef float AccumTrait;
+    };
+
+    template<>
+    class AccumulationTrait<double> {
+    public:
+        typedef double AccumTrait;
+    };
+    template<>
+    class AccumulationTrait<int> {
+    public:
+        typedef int AccumTrait;
+    };
+
+    template<typename T>
+    class SumPolicy;
+    template<typename T>
+    class SumPolicy {
+    public:
+        static void accumulate(T const& val, typename AccumulationTrait<T>::AccumTrait &total ) {
+            total += val;
+        }
+    };
+
+    template<typename T>
+    class MultPolicy;
+
+    template<typename T>
+    class MultPolicy {
+    public:
+        static void accumulate(T const& val, typename AccumulationTrait<T>::AccumTrait &total) {
+            total *= val;
+        }
+    };
+
+    template<typename T, typename Policy = SumPolicy<T>>
+    static typename AccumulationTrait<T>::AccumTrait accum(T* beg, T* end) {
+        static typename AccumulationTrait<T>::AccumTrait total = AccumulationTrait<T>::AccumTrait();
+        for (; beg != end; beg++) {
+            Policy::accumulate(*beg, total);
+        }
+        return total;
+    }
+
+    void driver() {
+        //vector<int> vi = { 1,2,3,4,5 };
+        //int sum = accum<int, MultPolicy<int>>(&vi[0], &vi[4]);
+        int num[] = { 1,2,3,4,5 };
+        accum<int,MultPolicy<int>>(&num[0], &num[5]);
+    }
+
+
+}
+namespace CH_15_2 {
+    
+    template <typename T>
+    class TypeSize {
+    public:
+        static size_t const Size = sizeof(T);
+    };
+    template<typename C>
+    class T {
+    public:
+        typedef typename C::value_type Type;
+    };
+    
+    template<typename C>
+    typename C::value_type sum_of_elem(C coll) {
+
+    }
+    template<typename COLL>
+    void printTypeName(COLL const& coll) {
+        //cout << "Type in container" << (ElementT<COLL>::Type).name();
+    }
+    void driver() {
+        vector<int> vi;
+        
+    }
+}
+namespace CPP_15_2_4_Promotraits {
+    template<bool C,typename T1,typename T2>
+    class IFTHENELSE;
+
+    template<typename T1, typename T2>
+    class IFTHENELSE<true,T1,T2>{
+    public:
+        typedef T1 ResultT;
+    };
+
+    template<typename T1,typename T2>
+    class IFTHENELSE<false, T1, T2> {
+    public:
+        typedef T2 ResultT;
+    };
+
+    template <class T1,class T2>
+    class PromoTrait {
+    public:
+        typedef typename IFTHENELSE < (sizeof(T1)>sizeof(T2)), T1, T2 > ::ResultT FinalResultType;
+
+    };
+
+    template<typename T>
+    class PromoTrait<T,T> {
+    public:
+        typedef T FinalResulType;
+    };
+
+}
+namespace CPP_15_3_4 {
+
+}
+namespace CheckClass {
+    template <typename T> 
+    class IsClassT {
+    public:
+        //typedef char One;
+        //typedef struct {char a[2] } Two;
+        //template<typename C> static One test<C>(int C::*);
+        //template<typename C> static Two test<C>(...);
+        //enum {Yes = (sizeof(IsClassT<T>::test<T>(0))==One) };
+        //enum { No = !Yes; };
+    };
+}
+namespace CPP15_3_1 {
+    template <bool C,typename T1,typename T2>
+    class IFTHENELSE;
+
+    template <typename T1, typename T2>
+    class IFTHENELSE<true, T1, T2> {
+    public:
+        typedef T1 Type;
+    };
+
+    template <typename T1, typename T2>
+    class IFTHENELSE<false, T1, T2> {
+    public:
+        typedef T2 Type;
+    };
+
+    template<typename T>
+    class Array {
+
+    };
+
+    template <typename T>
+    class RParam {
+    public:
+        typedef typename IFTHENELSE < (sizeof(T)>(2 * sizeof(void*))), T const&, T > ::Type ResultType;
+    };
+
+    template<typename T>
+    class RParam<Array<T>> {
+    public:
+        typedef Array<T> const& ResultType;
+    };
+
+    void driver() {
+    }
+}
+namespace CPP15_2_4{
+    template <typename T, int N>
+    std::array<T, N> operator+(std::array<T, N>& lhs, std::array<T, N>& rhs);
+
+    /*template <typename T, int N>
+    std::array<T, N> operator+(std::array<T, N>& lhs, std::array<T, N>& rhs) {
+        typename std::array<T, N>::iterator pos(lhs.begin());
+        typename std::array<T, N>::iterator end(lhs.end());
+        
+        typename std::array<T, N>::iterator pos_r(lhs.begin());
+        typename std::array<T, N>::iterator end_r(lhs.end());
+
+        std::array<T, N> res;
+        typename std::array<T, N>::iterator pos_res(lhs.begin());
+        typename std::array<T, N>::iterator end_res(lhs.end());
+
+        while (pos != end) {
+            *pos_res = *pos + *pos_r;
+            pos++;
+            pos_r++;
+            pos_res++;
+        }
+        return res;
+    }*/
+    class MyClass {
+        int n_;
+    public:
+        MyClass(int n) :n_(n) {
+
+        };
+        MyClass& operator+(MyClass const& rhs) {
+            n_ = n_ + rhs.n_;
+        };
+    };
+    void driver() {
+        std::array<int, 5> a1{ 1,2,3,4,5 };
+        std::array<int, 5> a2{ 10,20,30,40,50 };
+        std::array<int, 5> a3 = a1 + a2;
+        std::array<float, 5> f1{ 1,2,3,4,5 };
+        std::array<float, 5> f2{ 10,20,30,40,50 };
+        std::array<float, 5> f3 = f1 + f2;
+        std::array<MyClass, 5> m1{ 1,2,3,4,5 };
+        std::array<MyClass, 5> m2{ 10,20,30,40,50 };
+        std::array<MyClass, 5> m3 = m1 + m2;
+
+    }
+
+   
+}
+
 namespace CH_10 {
     class Sample {
     public:
@@ -144,10 +372,11 @@ namespace CH_10 {
 
     }
     void f2() {
-        typedef int X;
+        
+        using X = int;
         X a=5;
-        typedef char CH[10];
-        CH a={};
+        typedef char CH_ARRAY[10];
+        CH_ARRAY b;
         f1(12);
         typedef int ROWS[5];
         ROWS col[10];
@@ -190,13 +419,16 @@ namespace Sample_My_Smart_Ptr
         
     }
 }
+namespace CH_15 {
+
+}
 namespace CH_9_3 {
 
     template <typename T>
     class MyClass {
     public:
         struct X{
-        int a
+            int a;
         };
         X x;
     };
@@ -206,7 +438,7 @@ namespace CH_9_3 {
     public:
         void fun() {
             int p = 5;
-            p = mc.x.a* p;
+            //p = mc.x.a* p;
         };
     };
     template<>
@@ -233,17 +465,86 @@ RT1 test(typename T::X t) {}
 template <typename T>
 RT2 test(...) {}
 
-template <typename T, typename FUNC >
-void callFunc(T& obj, FUNC* fptr) {
+template<typename COLL>
+void printColl(COLL col) {
+    typename COLL::const_iterator pos(col.begin());
+    typename COLL::const_iterator end(col.end());
+    while (pos != end) {
+        cout << *pos << "\t";
+        pos++;
+    }
+    cout << endl;
+}
+
+template <typename COLL, typename FUNC >
+void callFunc(COLL& obj, FUNC* fptr) {
     
-    fptr<typename T::value_type>(obj);
+    fptr(obj);
 }
 
 void callFunc_driver() {
     vector<int> vi{ 1,2,3,4,5 };
     deque<int> di{ 1,2,3,4,5 };
-    callFunc(vi,&printColl_v);
-    callFunc(di,&printColl_d);
+    
+    callFunc(vi,&printColl<vector<int>>);
+    callFunc(di,&printColl<deque<int>>);
+}
+namespace OVERFLOW_SAMPLE {
+    char add(int n,char *arr) {
+        char t = arr[0];
+        int sum = arr[0];
+        
+        for (int i = 1; i < n; i++) {
+            t = t + arr[i];
+            sum = sum + (arr[i]);
+        }
+        return t;
+    }
+
+    void driver() {
+        vector<char> vc{ 'a','b','c','d','r' };
+        cout << add(vc.size(), vc.data());
+    }
+
+}
+namespace GET_MAX_OF_CONTAINER {
+    template <typename COLL>
+    typename COLL::value_type max_elem(COLL coll) {
+        typename COLL::iterator pos(coll.begin());
+        typename COLL::iterator end(coll.end());
+        typename COLL::value_type cur_max = *pos;
+        pos++;
+        while (pos != end) {
+            if (*pos > cur_max) {
+                cur_max = *pos;
+            }
+            pos++;
+        }
+        return cur_max;
+    }
+    template <typename COLL,typename max_comparator>
+    typename COLL::value_type max_elem(COLL coll) {
+        typename COLL::iterator pos(coll.begin());
+        typename COLL::iterator end(coll.end());
+        typename COLL::value_type cur_max = *pos;
+        pos++;
+        while (pos != end) {
+            cur_max = max_comparator(*pos, cur_max);
+            pos++;
+        }
+        return cur_max;
+    }
+    
+    void driver() {
+        vector<int> vi{ 1,2,3,4,5 };
+        deque<int> di{ 1,2,3,4,5 };
+        cout<<"max elem of vector"<<max_elem(vi)<<endl;
+        cout<<"max elem of deque"<<max_elem(di)<<endl;
+        typedef struct { char arr[3]; }type24;
+        type24 a={'a','b','c'};
+        
+    }
+
 }
 
 template<template <typename T> typename T2>
@@ -264,7 +565,7 @@ class C {};
 int C = 0;
 int main()
 {
-    callFunc_driver();
+    OVERFLOW_SAMPLE::driver();
     return 0;
     int i;
     Init(&i);
